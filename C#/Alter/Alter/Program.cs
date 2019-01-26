4bd039f3bab344041728fb3dc9e27f63630d220d
Galina Mihaylova <46779037+galina-mihaylova@users.noreply.github.com>
2019-01-26 12:51:27 +0100

    c#

---
+++ C#/Alter/Alter/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alter = 15;
+            Console.WriteLine("Wie alt bist du? (15-19)");
+            string eingabe = Console.ReadLine();
+
+            alter = Convert.ToInt32(eingabe);
+
+            if (alter < 16)
+            {
+                Console.WriteLine("Kein Alkohol für dich!");
+            }
+            else if (alter == 16)
+            {
+                Console.WriteLine("Nur Bier und Wein mein Freund!");
+            }
+            else if (alter == 17)
+            {
+                Console.WriteLine("Ein Jahr bis zum harten Alk!");
+            }
+            else
+            {
+                Console.WriteLine("Mach doch was du willst!");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
